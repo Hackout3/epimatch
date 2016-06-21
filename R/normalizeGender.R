32745e2d172502217213cfa4feede31722526eae
kplaney <catherine.planey@gmail.com>
2016-06-21 14:59:15 -0700

    added function to merge on a gender dictionary (after the user specifies the dictionary)

---
+++ R/normalizeGender.R
@@ -0,0 +1,13 @@
+
+#note: assumes that the gender dictionary contains all possible string values
+#found in this dataset. That's because we force the user to do this through
+#the shiny app.
+normalizeGender(genderDictionary, dataset,
+                datasetGenderString = "Gender",
+                genderDictionaryString = "originalGender"){
+  dataset <- merge(dataset, genderDictionary, by.x = datasetGenderString,
+                   by.y = genderDictionaryString,
+                   all = TRUE)
+  #"correct" gender is the "finalGender" column from the gender dictionary now
+  return(dataset)
+  }
