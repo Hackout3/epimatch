778535d9b79b20a274f898617b2c002a6b67fa1f
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-06-21 12:34:00 -0700

    create package infastructure

--- R/generic.R
+++ R/generic.R
@@ -1,12 +1,13 @@
-#' Title
+#' generic function for matching
 #'
-#' @param dat
-#' @param ...
+#' @param dat a vector of data to determine matching
+#' @param ...condiditions to apply
 #'
-#' @return
+#' @return a pairwise matrix of scores from 0 (exact match) to 1 (no match)
 #' @export
 #'
 #' @examples
+#' generic_match(1:3)
 generic_match <- function(dat, ...){
 	if (!any(duplicated(dat))){
 		return(0)
