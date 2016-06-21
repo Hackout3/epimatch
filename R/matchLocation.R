acb7bebf17d3809071d31e5abdb4670ec6749b55
John Lees <lees.john6@gmail.com>
2016-06-21 23:14:46 +0100

    Fix location variable names

--- R/matchLocation.R
+++ R/matchLocation.R
@@ -3,14 +3,14 @@
 
 # TODO: match even if fields out of order
 # TODO: split fields input by comma into columns
-locationDists <- function(locationDataFrame){
-  cleaned <- apply(locationDataFrame, 2, tolower)
+locationDists <- function(dat){
+  cleaned <- apply(dat, 2, tolower)
 
   # This returns a matrix with rowwise comparisons,
   # elements are the fraction of matching fields
-  n <- seq_len(nrow(locationDataFrame))
+  n <- seq_len(nrow(cleaned))
   id <- expand.grid(n, n)
-  dists <- matrix(rowSums(test[id[,1],] == test[id[,2],]), ncol=length(n))
+  dists <- matrix(rowSums(cleaned[id[,1],] == cleaned[id[,2],]), ncol=length(n))
   diag(dists) <- 0
 
   dists <- dists/ncol(cleaned)
