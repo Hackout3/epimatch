bde4b06c75559be6437b28be855c43bbc6231c44
kplaney <catherine.planey@gmail.com>
2016-06-21 13:22:42 -0700

    Make a helper function to find matching case IDs within a single dataset, just on the case ID vector.

---
+++ R/exactMatchCaseID.R
@@ -0,0 +1,21 @@
+
+#find matching case IDs within a single dataset
+#test: caseIDVector <- c("AB-10-1", "AB-10-5", "AB-10_1")
+exactMatchCaseIDIntraDataset <- function(caseIDVector){
+  #replace all puncutation with a lowercase character.
+  caseIDVector <- gsub("[[:punct:]]", "_", caseIDVector)
+  #make all characters lowercase
+  caseIDVector <- tolower(caseIDVector)
+  return(adist(caseIDVector, caseIDVector))
+}
+
+#input: two vectors of case IDs
+
+# exactMatchCaseID <- function(caseIDVector1, caseIDVector2){
+#   #replace all puncutation with a lowercase character.
+#   caseIDVector1 <- gsub("[[:punct:]]", "_", caseIDVector1)
+#   caseIDVector2 <- gsub("[[:punct:]]", "_", caseIDVector2)
+#   matchMatrix <- matrix(data = FALSE, nrow = length(caseIDVector1),
+#                         ncol = length(caseIDVector2))
+#   return()
+# }
