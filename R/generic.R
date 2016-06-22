e11d0a94b9ce2e2c74dd5f936cf6d240b2f38830
John Lees <lees.john6@gmail.com>
2016-06-22 21:53:09 +0100

    Add cleaning into generic

--- R/generic.R
+++ R/generic.R
@@ -10,9 +10,11 @@
 #' @examples
 #' generic_match(1:3)
 generic_match <- function(dat1, dat2=NULL, strings=F){
-	if (!is.null(dat2))
+	dat1 <- cleanString(dat1)
+
+  if (!is.null(dat2))
 	{
-	  dat1 <- rbind(dat1, dat2)
+	  dat1 <- rbind(dat1, cleanString(dat2))
 	}
 
   # String distance matching
@@ -29,4 +31,13 @@ generic_match <- function(dat1, dat2=NULL, strings=F){
     diag(dists) <- 0
   }
   return(dists/max(dists))
+}
+
+# This is duplicated - not how to share these functions in an R package?
+# Is this done automatically?
+cleanString <- function(dat)
+{
+  clean_dat <- apply(dat, 2, tolower)
+  clean_dat <- apply(clean_dat, 2, trimws)
+  return(data.frame(clean_dat, stringsAsFactors = F))
 }
