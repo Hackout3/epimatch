bee019f9f47f01a0cc5c6a90519f95c1d91e9861
kplaney <catherine.planey@gmail.com>
2016-06-22 14:00:03 -0700

    made code more efficient by just feeding in the number of rows in each dataset, instead of the datasets themselves

--- R/returnMatches.R
+++ R/returnMatches.R
@@ -1,6 +1,6 @@
 #'
 #'
-#' @param two datasets (ordered as they have been for all functions) and a distance matrix
+#' @param the number of rows for both datasets (ordered as they have been for all functions) and a distance matrix
 #'  thresh: distance threshold (smaller means more conservative matches)
 #' @return list where each index are the row indices for data rows in that cluster,
 #' and clusters are ordered from the highest to lowest match ranking
@@ -8,7 +8,7 @@
 #'
 #' @examples
 
-returnMatches <- function(d1, d2, distMatrix, thresh){
+returnMatches <- function(nRowD1, nRowD2, distMatrix, thresh){
   rowCount <- 1
   numMatchClust <- 1
   distMatrix[upper.tri(distMatrix, diag= TRUE)] <- NA
@@ -17,20 +17,17 @@ returnMatches <- function(d1, d2, distMatrix, thresh){
     indices <- which(distMatrix[, rowCount] > thresh)
     if(length(indices) > 1){
       numMatchClust <- numMatchClust + 1
-      if(!is.missing(d2)){
-        if(rowCount <= nrow(d1)){
-          indices1 <- indices[which(indices <= nrow(d1))]
-          indices2 <- indices[which(indices > nrow(d1))]
-         # matches <- rbind(d1[c(rowCount, indices1), ], d2[(indices2- nrow(d2)), ])
-          matchIndices[[numMatchClust]]$d1 <- indices1
+      if(!is.missing(nRowD2)){
+        if(rowCount <= nRowD1){
+          indices1 <- indices[which(indices <= nRowD1)]
+          indices2 <- indices[which(indices > nRowD1)]
+          matchIndices[[numMatchClust]]$nRowD1 <- indices1
           #must re-adjust indices to start at one for dataset 2
-          matchIndices[[numMatchClust]]$d2 <- indices2 - nrow(d2)
+          matchIndices[[numMatchClust]]$nRowD2 <- indices2 - nrow(nRowD2)
         }else{
-          #matches <- d2[c(rowCount, indices), ]
           matchIndices[[numMatchClust]] <- indices
         }
       }else{
-        #matches <- d2[c(rowCount, indices), ]
         matchIndices[[numMatchClust]] <- indices
       }
       #NA these out so that we don't compare them again
