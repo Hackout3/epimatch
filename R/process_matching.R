e1d0eb60672b4f82c6dfddd51de816dfde1ab80c
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-06-22 12:51:05 -0700

    add process_matching; no examples yet

---
+++ R/process_matching.R
@@ -0,0 +1,41 @@
+#' Process the functions used for matching data
+#'
+#' Input one or two data sets, process them with a list of containing functions,
+#' column names, and function parameters, and return a list of matrices by
+#' function.
+#'
+#' @param dat1 An input linelist
+#' @param dat2 An optional extra linelist
+#' @param funlist A list containing lists containing:
+#' \itemize{
+#'  \item d1vars - variable names for dataset 1
+#'  \item d2vars - variable names for dataset 2
+#'  \item fun - function name to process on these variables
+#'  \item extraparams - extra parameters that need to be applied with the function.
+#' }
+#'
+#' @return a list of distance matrices scaled by weight
+#'
+#' @export
+#'
+#' @examples
+#'
+process_matching <- function(dat1, dat2 = NULL, funlist = list()){
+  if (length(funlist) == 0){
+    stop("Please provide a list for the funlist parameter")
+  }
+  if (!is.data.frame(dat1) || !is.data.frame(dat2)){
+    stop("input datasets must be data frames")
+  }
+  out_matrices <- vector(mode = "list", length = length(funlist))
+  for (i in seq(funlist)){
+    param_list <- funlist[[i]]
+    d1 <- dat1[paramlist$d1vars, drop = FALSE]
+    d2 <- if (!is.null(dat2)) dat2[paramlist$d2vars, drop = FALSE] else NULL
+    out_matrices[[i]] <- do.call(fun, c(list(d1, d2), paramlist$extraparams))
+  }
+  if (!is.null(names(funlist))){
+    names(out_matrices) <- names(funlist)
+  }
+  return(out_matrices)
+}
