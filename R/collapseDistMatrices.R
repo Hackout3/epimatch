7444262c76ace509cce5776d00bdd908a6e177d1
kplaney <catherine.planey@gmail.com>
2016-06-22 13:40:48 -0700

    added dist2 as names

--- R/collapseDistMatrices.R
+++ R/collapseDistMatrices.R
@@ -9,16 +9,16 @@
 #' just sum two of the same matrices:
 #' caseIDVector <- c("AB-10-1", "AB-10-5", "AB-10_1")
 #' dist1 <- exactMatchCaseIDIntraDataset(caseIDVector)
-#' d2 <- exactMatchCaseIDIntraDataset(caseIDVector)
-#' summedDistMatrix <- collapseDistMatrices(dist1, d2, c(0.5, 0.5))
+#' dist2 <- exactMatchCaseIDIntraDataset(caseIDVector)
+#' summedDistMatrix <- collapseDistMatrices(dist1, dist2, c(0.5, 0.5))
 
-collapseDistMatrices <- function(dist1, d2, weightVector = c(0.5, 0.5)){
+collapseDistMatrices <- function(dist1, dist2, weightVector = c(0.5, 0.5)){
     if(any(is.na(dist1))){
       dist1[is.na(dist1)] <- 0
     }
-     if(any(is.na(d2))){
-      d2[is.na(d2)] <- 0
+     if(any(is.na(dist2))){
+      dist2[is.na(dist2)] <- 0
      }
-    dist1 <- dist1*(1-weightVector[1]) + d2*(1-weightVector[2])
+    dist1 <- dist1*(1-weightVector[1]) + dist2*(1-weightVector[2])
   return(dist1)
 }
