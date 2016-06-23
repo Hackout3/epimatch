df1e22d430fa4c7f5c30275f4c2e5008cd1827a8
kplaney <catherine.planey@gmail.com>
2016-06-23 10:26:48 -0700

    pre-allocate vector and order final matches by match score (lowest/closest first)

--- R/returnMatches.R
+++ R/returnMatches.R
@@ -7,20 +7,22 @@
 #' @export
 #'
 #' @examples
+#' caseIDVector <- c("AB-10-1", "AB-10-5", "AB-10_1")
+#' distMatrix <- adist(c(caseIDVector, caseIDVector), c(caseIDVector, caseIDVector))
+#' returnMatches(3, 3, distMatrix, thresh = 0.5)
 returnMatches <- function(nRowD1, nRowD2, distMatrix, thresh){
   rowCount <- 1
-  numMatchClust <- 0
   distMatrix[upper.tri(distMatrix, diag= TRUE)] <- NA
-  matchIndices <- list()
   numDistRow <- nrow(distMatrix)
+  matchIndices <- vector("list", numDistRow)
   while(any(distMatrix < thresh, na.rm = TRUE) && rowCount <= numDistRow){
     indices <- which(distMatrix[, rowCount] < thresh)
     score <- sum(distMatrix[indices, rowCount], na.rm = TRUE)
     if(length(indices) >= 1){
-      numMatchClust <- numMatchClust + 1
-      matchIndices[[numMatchClust]] <- list()
-      names(matchIndices)[numMatchClust] <- score
       if(!missing(nRowD2)){ #dealing with 2 datasets?
+          matchIndices[[rowCount]] <- vector("list",2)
+          names(matchIndices[[rowCount]]) <- c("d1", "d2")
+          names(matchIndices)[rowCount] <- score
           indices1 <- indices[which(indices <= nRowD1)]
           indices2 <- indices[which(indices > nRowD1)]
           #must also "tack on" the row index for this while loop:
@@ -29,20 +31,28 @@ returnMatches <- function(nRowD1, nRowD2, distMatrix, thresh){
           }else{
             indices2 <- c(rowCount, indices2)
           }
-          matchIndices[[numMatchClust]]$d1 <- indices1
-          if(length(indices2 > 0)){
+          matchIndices[[rowCount]][[1]] <- indices1
+          if(length(indices2) > 0){
             #must re-adjust indices to start at one for dataset 2
-            matchIndices[[numMatchClust]]$d2 <- indices2 - nrow(nRowD2)
+            matchIndices[[rowCount]][[2]] <- indices2 - nRowD2
           }else{ #just have this be blank like indices2 is
-            matchIndices[[numMatchClust]]$d2 <- indices2
+            matchIndices[[rowCount]][[2]] <- indices2
         }
       }else{
-        matchIndices[[numMatchClust]]$d1 <- c(rowCount, indices)
+        matchIndices[[rowCount]] <- vector("list",1)
+        names(matchIndices)[rowCount] <- score
+        matchIndices[[rowCount]][[1]] <- c(rowCount, indices)
+        names(matchIndices[[rowCount]]) <- c("d1")
       }
       #NA these out so that we don't compare them again
       distMatrix[indices, indices] <- NA
     }
     rowCount <- rowCount + 1
- }
+  }
+  matchIndices <- matchIndices[!is.na(names(matchIndices))]
+  #order with lowest score (closest match) first
+  matchIndices <- matchIndices[sort.int(as.numeric(names(matchIndices)),
+                                                   decreasing = FALSE,
+                                                   index.return= TRUE)$ix]
   return(matchIndices)
 }
