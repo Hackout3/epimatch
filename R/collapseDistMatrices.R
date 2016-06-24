846f432ff510969b9458b51f3bfe93517788971d
kplaney <catherine.planey@gmail.com>
2016-06-24 11:39:36 -0700

    added warnings if a matrix is not two-dimensional and then skips over it

--- R/collapseDistMatrices.R
+++ R/collapseDistMatrices.R
@@ -14,7 +14,8 @@
 #' m2 <- nameDists(caseIDVector)
 #' summedDistMatrix <- collapseDistMatrices(list(m1, m2), c(0.5, 0.5))
 
-collapseDistMatrices <- function(distMatrixList,weightVector = NULL){
+collapseDistMatrices <- function(distMatrixList,weightVector = NULL,
+                                 NAweight = 0.5){
   matlen <- length(distMatrixList)
   if(is.null(weightVector) || length(weightVector != matlen)){
     weightVector <- rep.int(1/(matlen), times = matlen)
@@ -22,20 +23,30 @@ collapseDistMatrices <- function(distMatrixList,weightVector = NULL){
   #don't want a weight vector of zeros if they are initially all one
   #this would result in all distances being zero.
   weightVector <- 1 - weightVector + (.Machine$double.eps)^0.5
-
+  numMatrices <- length(distMatrixList[[m]])
   for(m in 1:matlen){
-    #replace NAs with zeros
-    dM <- as.matrix(distMatrixList[[m]])
-    missing_data <- is.na(dM)
-    #set internal penalty of 0.5 for a missing value.
-    if(any(missing_data)){
-      dM[missing_data] <- 0.5
+    if(length(dim(distMatrixList[[m]])) != 2){
+      numMatrices <-  numMatrices -1
+      dM <- 0 #just make zero in case m==1; can add to other matrices later then.
+      warning(paste0("Distance matrix number ", m , " is not two-dimensional in the collapseDistMatrices function."))
+    }else{  #replace NAs with zeros
+      dM <- as.matrix(distMatrixList[[m]])
+      missing_data <- is.na(dM)
+      #set internal penalty of 0.5 for a missing value.
+      if(any(missing_data)){
+        dM[missing_data] <- numMatrices
+      }
     }
     if (m == 1){
       finalMatrix <- dM #*weightVector[m]
     } else {
       finalMatrix <- finalMatrix + (dM) #*weightVector[m])
     }
+  }
+  if(numMatrices >0){
+    finalMatrix <- finalMatrix/numMatrices
+  }else{
+    warning("No distance matrices returned in collapseDistMatrices function.")
   }
   return(finalMatrix)
 }
