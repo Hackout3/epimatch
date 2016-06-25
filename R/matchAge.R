a80cfbbe9ef394c764266cd1a16d385bfcf618d2
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-06-24 18:24:23 -0700

    update documentation for ageDists

--- R/matchAge.R
+++ R/matchAge.R
@@ -1,15 +1,16 @@
 #' Create distance from age column
 #'
-#' @param dat1 a data frame of ages
-#' @param dat2 a data frame of ages
-#' @param e the number of years difference allowed for two entries to be the same
+#' @param dat1,dat2 a data frame of ages
+#' @param e the number of years difference allowed for two entries to be the
+#'   same
 #' @param extra_column a named vector specifying the type of age represented
 #'
 #' @description This function will take one or two data frames containing ages
 #'   and create a distance matrix based off of the manhattan distance. Data can
 #'   be entered as a single column data frame assuming years or a two column
 #'   data frame with a specifier for months or year in the second column.
-#' @return a distance matrix of differences in age.
+#' @return a distance matrix of binary values where 0 indicates that two ages
+#'   are within the given threshold of each other and 1 indicates not.
 #' @export
 #'
 #' @examples
@@ -17,9 +18,16 @@
 #' x <- data.frame(age = rpois(5, 30), age_class = "YEAR")
 #' y <- data.frame(age = rpois(5, 18), age_class = "MONTH")
 #' dat <- rbind(x, y)
+#' # Assume everything is in years
 #' ageDists(dat[1])
+#'
+#' # Specify year and month
 #' ageDists(dat, extra_column = c(mo = "MONTH", yr = "YEAR"))
+#'
+#' # Input two data sets
 #' ageDists(x[1], y, extra_column = c(mo = "MONTH", yr = "YEAR"))
+#'
+#' # allow ages within three years of each other to match
 #' ageDists(dat, e = 3, extra_column = c(mo = "MONTH", yr = "YEAR"))
 ageDists <- function(dat1, dat2 = NULL, e = 1,
                      extra_column = c(mo = "months", yr = "years")){
