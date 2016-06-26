2254cb91dcab0e2c7e2c553cd0886bdc87b950e9
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-06-26 11:17:48 -0700

    Allow for mix of YY and YYYY in dateDists
    This was acheived by specifying a select_formats function.
    
    This also changes the default threshold to 0L, returning strictly matching dates without normalization.
    
    Documentation was also update to reflect the changes.

--- R/matchDate.R
+++ R/matchDate.R
@@ -14,18 +14,39 @@
 #' @importFrom lubridate parse_date_time
 #'
 #' @examples
-#' test <- c("Jan-21-01", "Jan-25-02", "Jan-21-01", "Jan-31-01")
-#' test <- data.frame(test, stringsAsFactors = FALSE)
+#'
+#' # Two data sets in Month-Day-Year and Year-Month-Day format, respectively.
+#' # Note that the way the dates can vary within data sets, but it's assumed
+#' # that the order within a data set is consistant.
+#'
+#' test1 <- data.frame(x = c("Jan-21-01", "01-25-02"), stringsAsFactors = FALSE)
+#' test2 <- data.frame(x = c("01-JAN-21", "2001-Jan-31"), stringsAsFactors = FALSE)
 #'
 #' # match dates within 11 days of each other
-#' dateDists(test, threshold = 11)
+#' dateDists(test1, test2, dat1Format = "mdy", dat2Format = "ymd", threshold = 11)
+#'
+#' # exact match of dates
+#' dateDists(test1, test2, dat1Format = "mdy", dat2Format = "ymd")
+#'
+#' # relative distance of dates by setting threshold to NULL
+#' dateDists(test1, test2, dat1Format = "mdy", dat2Format = "ymd", threshold = NULL)
+#'
 dateDists <- function(dat1, dat2 = NULL, dat1Format = "mdy", dat2Format = "mdy",
-                      threshold = NULL){
-  date_col <- lubridate::parse_date_time(dat1[[1]], dat1Format)
+                      threshold = 0L){
+  # Function for preprocessing of formats. Since a date with 2001 forces a date
+  # with 01 to be the year 0001, we need to modify the select_formats parameter
+  # for parse_date_time. I don't fully understand the mechanics of this, but the
+  # function below ensures that the two digit years are also in context of the
+  # four digit years.
+  sf <- function(trained){
+    n_fmts <- nchar(gsub("[^%]", "", names(trained))) + grepl("%y", names(trained))*1.5
+    names(trained[ which.max(n_fmts) ])
+  }
+  date_col <- lubridate::parse_date_time(dat1[[1]], dat1Format, select_formats = sf)
   # Read in a column, with defined format. If necessary combine with dat2
   if (!is.null(dat2))
   {
-    dat2     <- lubridate::parse_date_time(dat2[[1]], dat2Format)
+    dat2     <- lubridate::parse_date_time(dat2[[1]], dat2Format, select_formats = sf)
     date_col <- c(date_col, dat2)
   }
 
@@ -47,3 +68,4 @@ dateDists <- function(dat1, dat2 = NULL, dat1Format = "mdy", dat2Format = "mdy",
 
   return(dists)
 }
+
